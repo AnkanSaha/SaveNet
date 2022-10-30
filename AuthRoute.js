30592cb85a139016682e9f2476310c9210a247cf
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-10-30 15:24:17 +0530

    Add files via upload

--- AuthRoute.js
+++ AuthRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express.Router()
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Authentication = require('./Connector/AuthVerify')
+const Authentication = require('./Connector/AuthVerify');
 // setting up body parser
 app.use(bodyParser.urlencoded({extended: true, limit: '900mb', parameterLimit: 10000000}));
 app.use(bodyParser.json({limit: '900mb', parameterLimit: 10000000, extended: true}));
@@ -14,13 +14,13 @@ app.post('/CreateUser', (req, res)=>{
     var Email = req.body.Email
     var Country = req.body.Country
     var Password = req.body.Password
-    console.log(Name, Email, Country, Password)
-    Authentication.Registration(Name, Email, Country, Password, res)
-})
+    console.log(Name, Email, Country, Password);
+    Authentication.Registration(Name, Email, Country, Password, res);
+});
 // login
 app.post('/CheckUser', (req, res)=>{
     var Email = req.body.Email
     var Password = req.body.Password
-    Authentication.login(Email, Password, res)
-})
-module.exports = app
+    Authentication.login(Email, Password, res);
+});
+module.exports = app;
