09e821426c8859bb6b159e3aaded5588c19d8623
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-04 02:03:44 +0530

    Published January 2023 Version

---
+++ Database/Dashboard/dashboardModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+var DataQuery = {
+    Name: String,
+    Email: String,
+    Account_ID: String,
+    Password: String,
+    Title: String,
+    Description: String,
+    Date: String,
+}
+var QueryScheema = mongoose.Schema(DataQuery);
+module.exports.Query = mongoose.model("Data", DataQuery);
