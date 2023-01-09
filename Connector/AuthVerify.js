f21ada3d9f92d9b0a4f81713afae8d76aa7fcd37
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-10 01:53:21 +0530

    Re-written

---
+++ Connector/AuthVerify.js
@@ -0,0 +1,100 @@
+// Global variables
+const AuthModel = require("../Database/Auth/MongoAuthModel");
+const mongoose = require("mongoose");
+const MongoDBauthUrl = require("../Database/MongoDB_Config.js");
+const Backup = require("../backup/BackupConfig");
+
+//Registration
+async function Registration(Name, Email, Country, Password, res, Device_info) {
+  let { v1: uuidv1, v4: uuidv4 } = require("uuid");
+  let UniqueAccountID = uuidv4();
+  try {
+    await mongoose.connect(MongoDBauthUrl);
+    var tempdata = {
+      Account_ID: UniqueAccountID,
+      Name: Name,
+      Email: Email,
+      Country: Country,
+      Password: Password,
+      IsPaid: false,
+      Account_Create_Date: new Date(),
+      Account_Create_Time_Device_Info: Device_info,
+    };
+    var Registration = new AuthModel.AuthScheema(tempdata);
+    try {
+      var result = await AuthModel.AuthScheema.find({ Email: Email });
+      console.log(result);
+      if (result == "") {
+        try {
+          var AccountID = await AuthModel.AuthScheema.find({
+            Account_ID: UniqueAccountID,
+          });
+          console.log(AccountID);
+          if (AccountID == UniqueAccountID) {
+            UniqueAccountID = uuidv4();
+            res.status(404).json({
+              status: "Unable To Register, Please Try Again",
+            });
+          } else if (AccountID != UniqueAccountID) {
+            console.log("Account ID is Unique");
+            await Registration.save();
+            Backup.Registration(tempdata, UniqueAccountID);
+            console.log("Credential Saved");
+            res.status(200).json({
+              status: "User Successfully Registered",
+              AccountID: UniqueAccountID,
+              AccountCreateDate : tempdata.Account_Create_Date
+            });
+          }
+        } catch {
+          res
+            .status(404)
+            .json({ status: "Unable To Register, Please Try Again" });
+        }
+      } else if (result != "") {
+        res
+          .status(200)
+          .json({
+            status: "User Already Exist with this details, Please Login 😃",
+            AccountID: result[0].Account_ID,
+          });
+      }
+    } catch {
+      res.status(402).json({ status: "Unable To Register, Please Try Again" });
+    }
+  } catch {
+    res.status(502).json({ status: "Internal Server Error" });
+  }
+}
+
+// login
+async function login(Email, Password,AccountID, res) {
+  try {
+    await mongoose.connect(MongoDBauthUrl);
+    if(AccountID){
+      var loginresult = await AuthModel.AuthScheema.find({
+        Email: Email,
+        Account_ID: AccountID,
+      });
+    }
+    else if(Password){
+      var loginresult = await AuthModel.AuthScheema.find({
+        Email: Email,
+        Password: Password,
+      });
+    }
+    if (loginresult == "") {
+      res.status(404).json({ status: "User Not Registered" });
+      mongoose.connection.close();
+    } else if (loginresult != "") {
+      Backup.Login(loginresult[0]);
+      res.status(200).json({ status: loginresult });
+      mongoose.connection.close();
+    }
+  } catch {
+    console.log("Unknown Error Arrived");
+    res.status(500).json({ status: "Internal Server Error" });
+  }
+}
+module.exports.Registration = Registration;
+module.exports.login = login;
---
+++ data.js
@@ -0,0 +1,44 @@
+// importing require modules
+const express = require("express");
+const app = express();
+const port = 8000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routing = require("./router");
+const Authentication = require("./controller/AuthRoute");
+const DashRoute = require("./controller/ActionRoute.js");
+//setting up static folder
+app.use(express.static("src"));
+// setting up body parser
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "900mb",
+    parameterLimit: 10000000,
+  })
+);
+app.use(
+  bodyParser.json({ limit: "900mb", parameterLimit: 10000000, extended: true })
+);
+// setting up cors
+app.use(
+  cors([
+    "http://localhost:8000",
+    "http://localhost:4000",
+    "https://data.theankan.live",
+  ])
+);
+// setup router feature for api routes
+app.use(routing);
+app.use(Authentication);
+app.use(DashRoute);
+//  starting server
+app.listen(port,() => {
+  console.log(
+    `Server is running on port ${port} ` + "& current directory is " + __dirname
+  );
+});
+
+// view engine setup
+app.set('view engine', 'pug')
+app.set('views', './src/html')
