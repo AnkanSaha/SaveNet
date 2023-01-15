f39dfd2af91832ec193929e782c4aed6b2b1a325
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-15 23:51:35 +0530

    Changed DB

--- Database/Auth/MongoAuthModel.js
+++ Database/Auth/MongoAuthModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 let AuthScheemaDef = {
-  Account_ID: String,
-  Name: String,
-  Email: String,
-  Country: String,
-  IsPaid: Boolean,
-  Password: String,
-  Account_Create_Date: String,
-  Account_Create_Time_Device_Info: Object,
+  Account_ID: {type:String,required:true},
+  Name: {type:String,required:true},
+  Email: {type:String,required:true},
+  Country: {type:String,required:true},
+  IsPaid: {type:Boolean,default:false,required:true},
+  Password: {type:String,required:true},
+  Account_Create_Date: {type:String,required:true},
+  Account_Create_Time_Device_Info: {type:Object,required:false},
 };
 var AuthScheema = mongoose.Schema(AuthScheemaDef);
 module.exports.AuthScheema = mongoose.model("AuthDetails", AuthScheema);
--- Database/MongoDB_Config.js
+++ Database/MongoDB_Config.js
@@ -1,3 +1,4 @@
-const URL = 'mongodb+srv://Ankan157:Ankan1567@serverlessdb.q1ops.mongodb.net/SaveNet?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://Ankan157:Ankan1567@serverdb.q1ops.mongodb.net/SaveNet?retryWrites=true&w=majority';
 
 module.exports = URL;
+
