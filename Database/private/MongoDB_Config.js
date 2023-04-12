958bf16227f70e1ffac0eba111315146056c2a77
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-13 00:40:52 +0530

    Changed MongoDB Server

--- Database/private/MongoDB_Config.js
+++ Database/private/MongoDB_Config.js
@@ -1,4 +1,4 @@
-const URL = 'mongodb+srv://Ankan157:Ankan1567@serverdb.q1ops.mongodb.net/SaveNet?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://Ankan157:Ankan1567@db.q1ops.mongodb.net/SaveNet?retryWrites=true&w=majority';
 
 module.exports = URL;
 
