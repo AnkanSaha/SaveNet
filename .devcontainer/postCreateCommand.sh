9c980b3ec0cc475f2829184132f4cd76ad5c302c
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-01 17:49:01 +0000

    Fixed : Fixed Server Config

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y # Update apt-get cache
 sudo apt-get install -y neofetch # Install curl
 
 # Install Builder Dependencies
-npm i install --force # Install dependencies
+npm install --force # Install dependencies
 npm run buildcss # Build project
 
 # Setup Docker Containers
