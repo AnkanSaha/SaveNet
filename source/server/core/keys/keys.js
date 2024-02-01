d9dacc99a7021ec0c22df4dad38c0f2ba1001b78
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-01 22:27:44 +0530

    Fixed

--- source/server/core/keys/keys.js
+++ source/server/core/keys/keys.js
@@ -3,6 +3,6 @@ require('dotenv').config() // load .env variables
 // export the keys
 module.exports = {
   MONGO_URI: `${String(process.env.MONGO_URI)}${String(process.env.DB_NAME)}`,
-  PORT: Number(process.env.PORT) || 8000,
+  PORT: Number(process.env.PORT) ?? 8000,
   CORS_ORIGIN: String(process.env.CORS_ORIGIN)
 }
